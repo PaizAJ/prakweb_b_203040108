a4702a2f5b7819a0f2dab0e9f49bc8a18f33d8a1
John Doe <johndoe@example.com>
2022-10-14 00:50:56 +0700

    Database Wrapper

---
+++ mvc/app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_b_203040108/mvc/public');
+
+//DB
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvc');
